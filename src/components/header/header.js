a87b0bd2a2352cb35235111a5bf25005d411c8f3
ganesh nikale <ganesh.nikale@digitas.com>
2022-01-04 01:07:46 +0530

    header changes added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,35 +1,37 @@
 import React, { useEffect } from "react";
-import { googleSignIn, authUser,googleLogOut } from "../../store/usersAction";
+import { googleSignIn, authUser, googleLogOut } from "../../store/usersAction";
 import { useDispatch, useSelector } from "react-redux";
-import { Layout, Menu, Button } from "antd";
+import { Layout, Menu, Button, Image } from "antd";
 import { UserOutlined } from "@ant-design/icons";
-import { signGoogle, auth, provider, googleSignOut } from "../../utils/firebase";
+import {
+  signGoogle,
+  auth,
+  provider,
+  googleSignOut,
+} from "../../utils/firebase";
 
 const { Header } = Layout;
 
 const HeaderNav = () => {
   const dispatch = useDispatch();
-  const userAuth = useSelector(state => state.users.userAuthenticated);
- 
-  const userDetails =  useSelector(state => state.users.googleuserDetails);
+  const userAuth = useSelector((state) => state.users.userAuthenticated);
 
- auth.onAuthStateChanged((UserImpl) => {
-   console.log(UserImpl)
+  const userDetails = useSelector((state) => state.users.googleuserDetails);
+
+  console.log(userDetails);
+
+  auth.onAuthStateChanged((UserImpl) => {
+    console.log(UserImpl);
     if (UserImpl) {
-        dispatch(googleSignIn(UserImpl.providerData));
-        dispatch(authUser(true));
-        
-    } 
+      dispatch(googleSignIn(UserImpl.providerData));
+      dispatch(authUser(true));
+    }
 
     if (UserImpl === null) {
-        dispatch(googleLogOut('[{uid:0}]'))
-        dispatch(authUser(false));
-        
-    } 
-
-    
+      dispatch(googleLogOut("[{uid:0}]"));
+      dispatch(authUser(false));
+    }
   });
-
 
   return (
     <Layout>
@@ -38,33 +40,44 @@ const HeaderNav = () => {
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
           <Menu.Item key="1">All Projects</Menu.Item>
           <Menu.Item key="2">All Tasks</Menu.Item>
+
           <Menu.Item key="3">
-            <Button
-              type="primary"
-              shape="round"
-              onClick={signGoogle}
-              icon={<UserOutlined />}
-            >
-              Login
-            </Button>
+            {!userAuth ? (
+              <Button
+                type="primary"
+                shape="round"
+                onClick={signGoogle}
+                icon={<UserOutlined />}
+              >
+                Login
+              </Button>
+            ) : (
+              ""
+            )}
           </Menu.Item>
 
-          {(userAuth) ?<Menu.Item key="4">
-            hello 
-          </Menu.Item> : ''}
+          <Menu.Item key="4">
+            {userAuth ? `${userDetails[0].displayName}` : ""}
+          </Menu.Item>
+
+          <Menu.Item key="4">
+            {userAuth ? <Image preview={false} src={`${userDetails[0].photoURL}`} /> : ""}
+          </Menu.Item>
 
           <Menu.Item key="5">
-
-          <Button
-              type="primary"
-              shape="round"
-              onClick={googleSignOut}
-              icon={<UserOutlined />}
-            >
-              Logout
-            </Button>
+            {userAuth ? (
+              <Button
+                type="primary"
+                shape="round"
+                onClick={googleSignOut}
+                icon={<UserOutlined />}
+              >
+                Logout
+              </Button>
+            ) : (
+              ""
+            )}
           </Menu.Item>
-          
         </Menu>
       </Header>
     </Layout>
